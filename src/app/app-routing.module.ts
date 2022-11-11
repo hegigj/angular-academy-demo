95dc159f923e5afa60ce500dc23ae07d8cea4a57
hgjoka <hegi.gjoka@eng.it>
2022-11-11 18:34:48 +0100

    clear project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,13 @@
 import { NgModule } from '@angular/core';
-import {PreloadingStrategy, RouterModule, Routes} from "@angular/router";
-import {ListComponent} from "./list/list.component";
-import {RegisterFormComponent} from "./register-form/register-form.component";
-import {DetailsComponent} from "./list/details/details.component";
+import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: '/list',
-    pathMatch: 'full'
-  },
-  {
-    path: 'list',
-    component: ListComponent,
-    children: [
-      {
-        path: ':ID',
-        component: DetailsComponent
-      }
-    ]
-  },
-  // {
-  //   path: 'list/:id',
-  //   component: DetailsComponent
-  // },
-  {
-    path: 'register',
-    component: RegisterFormComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/list',
-    pathMatch: 'full'
-  }
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {
-      // preloadingStrategy: PreloadingStrategy
-    })
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [
     RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,24 @@ import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import {SharedModule} from "./shared/shared.module";
-import { FormComponent } from './form/form.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 import {DatePipe} from "@angular/common";
-import {format, FormatDatePipe} from './format-date.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
-import { RegisterFormComponent } from './register-form/register-form.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { DetailsComponent } from './list/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    FormComponent,
-    FormatDatePipe,
-    RegisterFormComponent,
-    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    SharedModule,
     HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [
-    DatePipe,
-    {
-      provide: format,
-      useValue: 'EEE dd/MM/yyyy hh:mm a'
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
